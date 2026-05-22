eb9fe76a932762903da19705f63a040cd16ca45e
Fernanda Akazawa <fernandakazawa@gmail.com>
2026-05-22 15:38:39 -0700

    code exercise 17

--- exercises/ex17.js
+++ exercises/ex17.js
@@ -1,6 +1,17 @@
 /*
-Write an anonymous function expression that stores a function in a variable called "laugh" and outputs the number of "ha"s that you pass in as an argument.
+Write an anonymous function expression that stores a function in a variable called "laugh" and outputs 
+the number of "ha"s that you pass in as an argument.
 
 laugh(3);
 Returns: hahaha!
 */
+
+const laugh = function (times) {
+  let answer = "";
+  for (let i = 0; i < times; i++) {
+    answer += "ha";
+  }
+  return answer + "!";
+};
+
+console.log(laugh(5));
