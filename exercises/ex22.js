78ac108f88e7c966596724a366dab060ce333d02
Fernanda Akazawa <fernandakazawa@gmail.com>
2026-05-25 14:35:11 -0700

    code exercise 22

--- exercises/ex22.js
+++ exercises/ex22.js
@@ -1,13 +1,22 @@
 /*
-James was creating an array with the colors of the rainbow, and he forgot some colors. The standard rainbow colors are usually listed in this order:
+James was creating an array with the colors of the rainbow, and he forgot some colors. 
+The standard rainbow colors are usually listed in this order:
 
 var rainbow = ["Red", "Orange", "Yellow", "Green", "Blue", "Purple"];
 but James had this:
 
 var rainbow = ["Red", "Orange", "Blackberry", "Blue"];
-Using only the splice() method, insert the missing colors into the array, and remove the color "Blackberry" by following these steps:
+Using only the splice() method, insert the missing colors into the array, and remove the color "Blackberry" 
+by following these steps:
 
 Remove "Blackberry"
 Add "Yellow" and "Green"
 Add "Purple"
 */
+
+const rainbow = ["Red", "Orange", "Blackberry", "Blue"];
+rainbow.splice(2, 1);
+rainbow.splice(2, 0, "Yellow");
+rainbow.splice(3, 0, "Green");
+rainbow.splice(rainbow.length, 0, "Purple");
+console.log(rainbow);
