cbf0c9d2a76b487d6eba199dea1468e228a506d7
Fernanda Akazawa <fernandakazawa@gmail.com>
2026-05-25 16:04:22 -0700

    code exercise 28

--- exercises/ex28.js
+++ exercises/ex28.js
@@ -11,3 +11,34 @@ deleteMessage(index) - removes the message corresponding to the index provided
 addFriend() - increases the friend count by 1
 removeFriend() - decreases the friend count by 1
 */
+
+const facebookProfile = {
+  name: "Fernanda",
+  numFriends: 100,
+  messages: [
+    "Good morning Sae!",
+    "This kitty is soooo cuuuuuuteteeee!!",
+    "This restaurant is awfull. Bad food and bad customer service",
+    "Happy birthday my dear friend Dani! Love you! <3",
+  ],
+
+  postMessage: function (message) {
+    this.messages.push(message);
+    return console.log(this.messages);
+  },
+  deleteMessage: function (index) {
+    this.messages.splice(index, 1);
+    return console.log(this.messages);
+  },
+  addFriend: function () {
+    return (this.numFriends += 1);
+  },
+  removeFriend: function () {
+    return (this.numFriends -= 1);
+  },
+};
+
+console.log(facebookProfile.addFriend());
+console.log(facebookProfile.removeFriend());
+facebookProfile.postMessage("I love Javascript!");
+facebookProfile.deleteMessage(2);
