44e8667ac537e18d9c1407ff1acdb8ad79a6e1ec
Fernanda Akazawa <fernandakazawa@gmail.com>
2026-05-25 15:32:57 -0700

    code exercise 25

--- exercises/ex25.js
+++ exercises/ex25.js
@@ -8,5 +8,22 @@ For example, say we called the function with the following arguments.
 
 loopy([15, 90], [2, 5], ["Batty", "Beacon"]);
 
-The loopy function should log to the console all the numbers from 15 to 90, except replacing multiples of 2 with "Batty", multiples of 5 with "Beacon", and multiples of 2 and 5 with "BattyBeacon".
+The loopy function should log to the console all the numbers from 15 to 90, except replacing multiples of 2 
+with "Batty", multiples of 5 with "Beacon", and multiples of 2 and 5 with "BattyBeacon".
 */
+
+const loopy = function (range, multiples, words) {
+  for (let i = range[0]; i <= range[1]; i++) {
+    if (i % multiples[0] === 0 && i % multiples[1] === 0) {
+      console.log(words[0] + words[1]);
+    } else if (i % multiples[0] === 0) {
+      console.log(words[0]);
+    } else if (i % multiples[1] === 0) {
+      console.log(words[1]);
+    } else {
+      console.log(i);
+    }
+  }
+};
+
+loopy([2, 70], [3, 7], ["Batty", "Beacon"]);
